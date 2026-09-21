db3710d476072abc4549ac524d3edaa6f2185667
James Anstey <james.anstey@ec.gc.ca>
2026-09-21 04:47:19 +0000

    allow path substitution in compare_mapfiles.py

--- scripts/compare_mapfiles.py
+++ scripts/compare_mapfiles.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 '''
 Testing generate_mapfiles.py
 
@@ -7,11 +8,26 @@ Load mapfiles from one dir, compare the contents of mapfiles with same filenames
 import os
 from pathlib import Path
 
-path = Path('mapfiles_gen')
+
+use_path_switch = True
+
+PATH_SWITCH = {
+    '/space/hall7/sitestore/eccc/crd/cccma/model_output/CMIP7/final': '/CCCMA_NFS/esg/esg_ng'
+}
+def do_path_switch(lines):
+    for k,line in enumerate(lines):
+        for old,new in PATH_SWITCH.items():
+            if old in line and line.count(old) == 1:
+                lines[k] = line.replace(old,new)
+
+
+# path = Path('mapfiles_orig_path')
+path = Path('mapfiles_server_path')
 
 filenames = os.listdir(path)
 
-path2 = Path('mapfiles_esgmapfile')
+# path2 = Path('mapfiles_esgmapfile')
+path2 = Path('mapfiles')
 filenames2 = os.listdir(path2)
 
 for filename in filenames:
@@ -32,6 +48,10 @@ for filename in filenames:
     assert len(lines) == len(lines2)
     assert len(set(lines)) == len(set(lines2))
 
+    if use_path_switch:
+        do_path_switch(lines)
+        do_path_switch(lines2)
+
     ok = True
     if lines != lines2:
         print('lines differ')
@@ -49,4 +69,5 @@ for filename in filenames:
 
     if ok:
         print('* mapfiles agree *')
-        
+    else:
+        raise Exception('* mapfiles DO NOT agree *')
