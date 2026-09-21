66037f14bfa3e88c4c3d83b7a3cf47f210a0c9c5
James Anstey <james.anstey@ec.gc.ca>
2026-09-21 06:41:59 +0000

    tidy up compare_mapfiles.py to take command-line args

--- scripts/compare_mapfiles.py
+++ scripts/compare_mapfiles.py
@@ -5,11 +5,12 @@ Testing generate_mapfiles.py
 Load mapfiles from one dir, compare the contents of mapfiles with same filenames in another
 '''
 
+import argparse
 import os
+
 from pathlib import Path
+from textwrap import dedent
 
-
-use_path_switch = True
 
 PATH_SWITCH = {
     '/space/hall7/sitestore/eccc/crd/cccma/model_output/CMIP7/final': '/CCCMA_NFS/esg/esg_ng'
@@ -20,54 +21,79 @@ def do_path_switch(lines):
             if old in line and line.count(old) == 1:
                 lines[k] = line.replace(old,new)
 
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description=dedent('''\
+            Compare mapfiles in two directories.
 
-# path = Path('mapfiles_orig_path')
-path = Path('mapfiles_server_path')
+            Comparison is done for all files present in the FIRST directory.
+            An Exception is raised if any disagree.
+            ''')
+    )
+    parser.add_argument('dir1', type=str,
+                        help='first directory - only filenames in this dir will be checked')
+    parser.add_argument('dir2', type=str,
+                        help='second directory')
 
-filenames = os.listdir(path)
+    parser.add_argument('-ps', '--path-switch', action='store_true', default=False,
+                        help='use path substitution for the comparison')
 
-# path2 = Path('mapfiles_esgmapfile')
-path2 = Path('mapfiles')
-filenames2 = os.listdir(path2)
+    return parser.parse_args()
 
-for filename in filenames:
-    print(f'\nChecking mapfile {filename}')
+def main():
+    args = parse_args()
+    use_path_switch = args.path_switch
 
-    if filename not in filenames2:
-        print(f'--> File not found in {path2}, skipping')
-        continue
+    path1 = Path(args.dir1)
+    filenames1 = os.listdir(path1)
 
-    with open(path / filename) as f:
-        w = f.read()
-    lines = w.split('\n')
+    path2 = Path(args.dir2)
+    filenames2 = os.listdir(path2)
 
-    with open(path2 / filename) as f:
-        w = f.read()
-    lines2 = w.split('\n')
+    for filename in filenames1:
+        if not filename.endswith('.map'):
+            continue
+        print(f'\nChecking mapfile {filename}')
 
-    assert len(lines) == len(lines2)
-    assert len(set(lines)) == len(set(lines2))
+        if filename not in filenames2:
+            print(f'--> File not found in {path2}, skipping')
+            continue
 
-    if use_path_switch:
-        do_path_switch(lines)
-        do_path_switch(lines2)
+        with open(path1 / filename) as f:
+            w = f.read()
+        lines = w.split('\n')
 
-    ok = True
-    if lines != lines2:
-        print('lines differ')
-        lines = sorted(lines)
-        lines2 = sorted(lines2)
-        if lines == lines2:
-            print('after sorting, lines are the same')
-            ok = True
-        else:
-            print('lines still differ after sorting')
+        with open(path2 / filename) as f:
+            w = f.read()
+        lines2 = w.split('\n')
+
+        assert len(lines) == len(lines2)
+        assert len(set(lines)) == len(set(lines2))
+
+        if use_path_switch:
+            do_path_switch(lines)
+            do_path_switch(lines2)
+
+        ok = True
+        if lines != lines2:
+            print('lines differ')
+            lines = sorted(lines)
+            lines2 = sorted(lines2)
+            if lines == lines2:
+                print('after sorting, lines are the same')
+                ok = True
+            else:
+                print('lines still differ after sorting')
+                ok = False
+        if set(lines) != set(lines2):
+            print('line contents differ')
             ok = False
-    if set(lines) != set(lines2):
-        print('line contents differ')
-        ok = False
 
-    if ok:
-        print('* mapfiles agree *')
-    else:
-        raise Exception('* mapfiles DO NOT agree *')
+        if ok:
+            print('* mapfiles agree *')
+        else:
+            raise Exception('* mapfiles DO NOT agree *')
+
+if __name__ == '__main__':
+    main()
+
